4e4a772338f432b0df676887141bfaa99f393527
abbacdef <coincash@mail.ru>
2018-12-11 16:07:39 +0500

    paper clip gem and uploading pictures

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'jquery-rails'
 gem 'sdoc'
+gem 'paperclip'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_10_160752) do
+ActiveRecord::Schema.define(version: 2018_12_11_095105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,10 @@ ActiveRecord::Schema.define(version: 2018_12_10_160752) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.integer "user_id"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.bigint "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_pics_on_user_id"
   end
 
